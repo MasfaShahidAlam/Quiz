a9e8b8865373c079a179daf303239f4c8fec11b3
Masfa Shahid Alam <masfaalam02@gmail.com>
2024-08-17 18:15:20 +0500

    j

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var num = prompt("Enter your Roll no")
 alert("Welcome"+ " "+ username +" "+"Best of Luck for your Quiz!");
 
 //-----------------------------Question No:1-----------------------------//
-var ques1 = prompt("What does HTML stand for? A). Hyper text markup language, B). Hyper Links,  D). Home tool markup language ")
+var ques1 = prompt("What does HTML stand for? A). Hyper Text MarkUp Language, B). Hyper Text MakeUp Language,  C).  Hyper Title MarkUp Language ")
 var correct1 = "a"
 if(ques1 === correct1){
     alert("Correct Answer!")
@@ -14,7 +14,7 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:2-----------------------------//
-var ques2 = prompt("Who is making the Web standards? A). Google, B). Mozilla, C). The World Wide Web Consortium, D). Microsoft")
+var ques2 = prompt("Which of the following is the correct file extension for HTML files? A). .htm, B). Both .htm and .html, C). .html,")
 var correct2 = "c"
 if(ques2 === correct2){
     alert("Correct Answer!")
@@ -23,7 +23,7 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:3-----------------------------//
-var ques3 = prompt("Choose the correct HTML element for the largest heading: A). <h1>, B). <heading>, C). <h6>, D). <head>")
+var ques3 = prompt("Choose the correct HTML element for the largest heading: A). <h1>, B). <heading>, C). <h6>")
 var correct3 = "a"
 if(ques3 === correct3){
     alert("Correct Answer!")
@@ -32,7 +32,7 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:4-----------------------------//
-var ques4 = prompt("What is the correct HTML element for inserting a line break? A). <ib>, B). <br>, C). <rb>, D). <break>")
+var ques4 = prompt("What is the correct HTML element for inserting a line break? A). <hr>, B). <br>, C). <break>")
 var correct4 = "b"
 if(ques4 === correct4){
     alert("Correct Answer!")
@@ -41,8 +41,8 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:5-----------------------------//
-var ques5 = prompt("Choose the correct HTML element to define important text. A). <b>, B). <important>, C). <i>, D). <strong>")
-var correct5 = "d"
+var ques5 = prompt(" Which HTML tag is used to create a hyperlink? A). <a>, B). <link>, C). <href>")
+var correct5 = "c"
 if(ques5 === correct5){
     alert("Correct Answer!")
 }
@@ -50,7 +50,7 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:6-----------------------------//
-var ques6 = prompt("Choose the correct HTML element to define emphasized text. A). <em>, B). <i>, C). <italic>, D). <strong>")
+var ques6 = prompt("How do you create a paragraph in HTML? A). <p>, B). <para>, C). <paragraph>")
 var correct6 = "a"
 if(ques6 === correct6){
     alert("Correct Answer!")
@@ -59,8 +59,8 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:7-----------------------------//
-var ques7 = prompt("Which character is used to indicate an end tag? A). *, B). ^, C). /, D). >")
-var correct7 = "c"
+var ques7 = prompt("Which tag is used to create an unordered list in HTML? A). ol, B). ul, C). li")
+var correct7 = "b"
 if(ques7 === correct7){
     alert("Correct Answer!")
 }
@@ -77,7 +77,7 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:9-----------------------------//
-var ques9 = prompt("How can you make a numbered list? A).<list>, B). <ul>, C). <ol>, D).<dl>")
+var ques9 = prompt("Which tag is used to define the metadata of an HTML document? A).<data>, B). <head>, C). <meta>")
 var correct9 = "c"
 if(ques9 === correct9){
     alert("Correct Answer!")
@@ -86,8 +86,8 @@ else{
     alert("Incorrect Answer!")
 }
 //-----------------------------Question No:10-----------------------------//
-var ques10 = prompt("How can you make a bulleted list? A).<list>, B). <ul>, C). <ol>, D).<dl>")
-var correct10 = "b"
+var ques10 = prompt("What does the hr tag represent in HTML? A). A horizontal rule line, B). A heading, C). A hyperlink")
+var correct10 = "a"
 if(ques10 === correct10){
     alert("Correct Answer!")
 }
